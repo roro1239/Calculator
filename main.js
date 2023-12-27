78bf3e32c297c246ea43c69e60aa5873b15d7b2b
roro1239 <sandhir631@gmail.com>
2023-12-27 11:15:06 -0500

    created operator function to detect which operator would do which calculator function

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-const add = function(num1 ,num2){
-return num1 + num2;
+let num1;
+let operators;
+let num2;
+const add = function (num1, num2) {
+    return num1 + num2;
 }
 
-const subtract = function(num1 ,num2){
-return num1 - num2;
+const subtract = function (num1, num2) {
+    return num1 - num2;
 }
 
-const multiply = function(num1 ,num2){
-return num1 * num2;
+const multiply = function (num1, num2) {
+    return num1 * num2;
 }
 
-const divide = function(num1 ,num2){
-    if (num2 ===0){
-        return "OPPS";
+const divide = function (num1, num2) {
+    return num1 / num2;
+}
+
+function operator(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null;
+            else return divide(a, b);
+        default:
+            return null;
     }
-    return num1/num2;
 }
-console.log(add(1,2));
-console.log(subtract(3,4));
-console.log(multiply(5,6));
-console.log(divide(1,16));
-console.log(divide(1,0));
-//switch (+) (-) (/) (*)
