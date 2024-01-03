fd9adf9edb1a5a67b1d8a292031283be658607ee
roro1239 <sandhir631@gmail.com>
2024-01-03 15:53:09 -0500

    calculator evaluates all the operations and works with dots

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let num1='';
-let operators=null;
-let num2='';
+let num1 = '';
+let operators = null;
+let num2 = '';
 
 let num = document.querySelectorAll(".number");
 let operatorButton = document.querySelectorAll(".operator");
@@ -11,59 +11,73 @@ let btnEqual = document.getElementById("btnEqual");
 let btnDelete = document.getElementById("btnDelete");
 let btnDot = document.getElementById("btnDot");
 
-//btnEqual.addEventListener('click',evaluate);
-btnClear.addEventListener('click',clear);
-btnDelete.addEventListener('click',deleting);
-btnDot.addEventListener('click',dot);
-//window.addEventListener('keydown',keyboardInput);
+btnEqual.addEventListener('click', evaluate);
+btnClear.addEventListener('click', clear);
+btnDelete.addEventListener('click', deleting);
+btnDot.addEventListener('click', dot);
+window.addEventListener('keydown',keyboardInput);
 
-num.forEach(button =>{
-    button.addEventListener('click',() =>{
+num.forEach(button => {
+    button.addEventListener('click', () => {
         appendNumbers(button.textContent);
- });
- });
-
- operatorButton.forEach(button =>{
-    button.addEventListener("click",()=>{
-        appendOperator(button.textContent);
-    
     });
 });
 
- function appendNumbers (input){
-    if(currentScreen.textContent === "0"){
-        currentScreen.textContent ='';
+operatorButton.forEach(button => {
+    button.addEventListener("click", () => {
+        appendOperator(button.textContent);
+
+    });
+});
+
+function appendNumbers(input) {
+    if (currentScreen.textContent === "0") {
+        currentScreen.textContent = '';
     }
     currentScreen.textContent += input;
 }
-function appendOperator(op){
-   /* if(operators !== null){
-        
-      }*/
+function appendOperator(op) {
+    if (operators !== null) {
+        evaluate();
+    }
     num1 = currentScreen.textContent;
-    operators=op;
-    lastScreen.textContent = num1 +" "+ operators;
-    currentScreen.textContent ='';
+    operators = op;
+    lastScreen.textContent = num1 + " " + operators;
+    currentScreen.textContent = '';
+}
+function evaluate() {
+    if (operators === null) return;
+    if (operators === '/' && currentScreen.textContent === '0') {
+        alert("can't divide by 0 !!!");
+        return;
+    }
+    num2 = currentScreen.textContent;
+    currentScreen.textContent = Math.round(operator(operators, num1, num2) * 10000) / 10000;
+    lastScreen.textContent = num1 + " " + operators + " " + num2;
+    operators = null;
 }
 function clear() {
     num1 = ''
     operators = null;
     num2 = '';
-    currentScreen.textContent ='0';
-    lastScreen.textContent='';
+    currentScreen.textContent = '0';
+    lastScreen.textContent = '';
 }
 
-function deleting(){
-    currentScreen.textContent = currentScreen.textContent.slice(0,-1);
+function deleting() {
+    currentScreen.textContent = currentScreen.textContent.slice(0, -1);
 }
 
-function dot(){
-    if(currentScreen.textContent ===''){
+function dot() {
+    if (currentScreen.textContent === '') {
         currentScreen.textContent = '0';
     }
-    if(currentScreen.textContent.includes('.')) return;
-    currentScreen.textContent +='.';
+    if (currentScreen.textContent.includes('.')) return;
+    currentScreen.textContent += '.';
 }
+/*function keyboardInput(e){
+
+}*/
 const add = function (num1, num2) {
     return num1 + num2;
 }
